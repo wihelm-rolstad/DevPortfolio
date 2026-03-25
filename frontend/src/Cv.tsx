10fd126f76b60a211ddab774730c1d7c81cf02a8
Wilhelm Svensgaard Rolstad <wilhelsr@stud.ntnu.no>
2026-03-25 12:25:27 +0100

    created git issue card and an issue for each experience

--- frontend/src/Cv.tsx
+++ frontend/src/Cv.tsx
@@ -1,8 +1,8 @@
 import { memo } from 'react'
 import { usePageSeo } from './hooks/usePageSeo'
 import { FileDown } from 'lucide-react'
+import GitHubCard from './components/GitHubCard'
 import AnimatedBox from './components/AnimatedBox'
-import Spline from '@splinetool/react-spline';
 
 const CV = () => {
     usePageSeo({
@@ -17,64 +17,79 @@ const CV = () => {
 
     return(
         <>
-            <div className="flex flex-col mx-auto rounded p-2 m-2 max-w-7xl overflow-hidden">
-                <AnimatedBox className="overflow-hidden p-10 h-115 text-[var(--text-color]">
-                {({ isVisible }) => (
-                    <div className="flex">
-                        <p className={`w-[50%] transform transition duration-2000 ${isVisible ? 'translate-x-0' : '-translate-x-[100px]'}`}>
-                            Studiespesialisering Stabekk VGS
-                        </p>
-                        <div className={`w-[50%] h-140 rounded-lg transform transition duration-2000 ${isVisible ? 'translate-x-0' : 'translate-x-[1000px]'}`}>
-                            <Spline scene="https://prod.spline.design/XfdUGJw97P4prxPG/scene.splinecode" />
-                        </div>
-                    </div>
-                )}
+            <div className="flex flex-col gap-20 mx-auto rounded p-2 m-2 max-w-7xl overflow-hidden">
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Videregående skole"
+                    description="Studiespesialiserende innenfor realfag og økonomi"
+                    date={new Date("2019-08-01")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["good first issue", "Size: Large", "Priority: High", "Medium", "MVP"]}
+                    project="grunnskole"
+                    milestone="studie"
+                    />
                 </AnimatedBox>
 
-                <AnimatedBox className="overflow-hidden p-10 h-115 text-[var(--text-color]">
-                {({ isVisible }) => (
-                    <div className="flex">
-                        <div className={`w-[50%] transform transition duration-2000 ${isVisible ? 'translate-x-0' : '-translate-x-[100px]'}`}>
-                            <h1>Førstegangstjeneste</h1>
-                            <p>Vaktsoldat HMKG</p>
-                            <p> AUG 2022 - JUN 2023</p>
-                        </div>
-                        <div className={`w-[50%] h-140 rounded-lg transform transition duration-2000 ${isVisible ? 'translate-x-0' : 'translate-x-[1000px]'}`}>
-                            <Spline scene="https://prod.spline.design/XfdUGJw97P4prxPG/scene.splinecode" />
-                        </div>
-                    </div>
-                )}
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Førstegangstjeneste"
+                    description="Vaktsoldat Garden"
+                    date={new Date("2022-08-05")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["Enhancement", "Size: Medium", "Priority: Critical", "Hard", "Performance"]}
+                    project="..."
+                    milestone="..."
+                    />
                 </AnimatedBox>
 
-                 <AnimatedBox className="overflow-hidden p-10 h-115 text-[var(--text-color]">
-                {({ isVisible }) => (
-                    <div className="flex">
-                        <div className={`flex flex-col w-[50%] transform transition duration-2000 ${isVisible ? 'translate-x-0' : '-translate-x-[100px]'}`}>
-                            <p>Bachelor i Informatikk NTNU</p>
-                            <p>Studiespesialisering Stabekk VGS</p>
-                            <p> AUG 2024 -</p>
-                        </div>
-                        <div className={`w-[50%] h-140 rounded-lg transform transition duration-2000 ${isVisible ? 'translate-x-0' : 'translate-x-[1000px]'}`}>
-                            <Spline scene="https://prod.spline.design/XfdUGJw97P4prxPG/scene.splinecode" />
-                        </div>
-                    </div>
-                )}
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Butikkmedarbeider"
+                    description="Spar Snarøya"
+                    date={new Date("2024-06-01")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["Enhancement", "Size: Small", "Priority: Medium", "Easy"]}
+                    project="erfaringer"
+                    milestone="..."
+                    />
                 </AnimatedBox>
 
-                <AnimatedBox className="overflow-hidden p-10 h-115 text-[var(--text-color]">
-                {({ isVisible }) => (
-                    <div className="flex">
-                        <div className={`flex flex-col w-[50%] transform transition duration-2000 ${isVisible ? 'translate-x-0' : '-translate-x-[100px]'}`}>
-                            <h1 className="text-3xl">Skaderådgiver Storebrand Forsikring</h1>
-                            <p>Sesongbasert Vikariat</p>
-                            <p> JUN 2025 -</p>
-                        </div>
-                        <div className={`w-[50%] h-140 rounded-lg transform transition duration-2000 ${isVisible ? 'translate-x-0' : 'translate-x-[1000px]'}`}>
-                            <Spline scene="https://prod.spline.design/XfdUGJw97P4prxPG/scene.splinecode" />
-                        </div>
-                    </div>
-                )}
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Bachelor Informatikk NTNU"
+                    description="Bachelor i Informatikk ved Norges teknisk- naturvitenskapelige universitet"
+                    date={new Date("2024-08-01")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["Enhancement", "Size: Large", "Priority: Critical", "Hard"]}
+                    project="bachelor"
+                    milestone="karriere"
+                    />
                 </AnimatedBox>
+                
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Skaderådgiver Storebrand"
+                    description="Skaderådgiver for motorskader i Storebrand Forsikring"
+                    date={new Date("2025-06-01")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["Enhancement", "Size: Medium", "Priority: High", "Medium"]}
+                    project="erfaringer"
+                    milestone="..."
+                    />
+                </AnimatedBox>
+
+                <AnimatedBox>
+                    <GitHubCard
+                    title="Nettsideutvikler DotDagene"
+                    description="Opprettholde og videreutvikle nettside for DotDagene, et verv som arrangerer karrieredag for it-studenter ved NTNU"
+                    date={new Date("2025-06-01")}
+                    assignee = "wilhelm-rolstad"
+                    labels={["Enhancement", "Size: Medium", "Priority: High", "Medium"]}
+                    project="erfaringer"
+                    milestone="karriere"
+                    />
+                </AnimatedBox>
+
 
                 <div className="cursor-pointer flex flex-row items-center gap-1 hover:scale-105 active:scale-95 transition duration-200 mx-auto">
                     <p>Download CV </p><FileDown strokeWidth={1.2}/>
---
+++ frontend/src/components/GitHubCard.tsx
@@ -0,0 +1,131 @@
+type GitHubCardProps={
+    title: string;
+    date: Date;
+    description: string;
+    assignee: string;
+    labels: string[];
+    imagePath?: string;
+    project?: string;
+    milestone?: string;
+}
+
+function GitHubCard({
+    title, 
+    date, 
+    description, 
+    assignee, 
+    labels,
+    imagePath,
+    project,
+    milestone
+}:GitHubCardProps){
+
+    const infoContainer="flex flex-col gap-2 justify-center"
+    const hrStyle ="border-gray-200"
+    const labelStyle = "font-bold text-gray-500"
+    const labelStyles: Record<string, { bg: string; text: string }> = {
+        "enhancement": { bg: "bg-cyan-500", text: "white" },
+
+        "priority: critical": { bg: "bg-red-600", text: "white" },
+        "priority: high": { bg: "bg-orange-400", text: "black" },
+        "priority: medium": { bg: "bg-yellow-300", text: "black" },
+        "priority: small": { bg: "bg-green-300", text: "black" },
+
+        "size: large": { bg: "bg-violet-600 ", text: "white" },
+        "size: medium": { bg: "bg-indigo-300 ", text: "lack" },
+        "size: small": { bg: "bg-sky-200 ", text: "black" },
+
+        "hard": { bg: "bg-rose-600", text: "white" },
+        "medium": { bg: "bg-amber-400", text: "black" },
+        "easy": { bg: "bg-lime-300", text: "black" },
+
+        "mvp": { bg: "bg-fuchsia-500", text: "white" },
+        "performance": { bg: "bg-emerald-500", text: "white" },
+        "good first issue" : { bg: "bg-indigo-500", text: "white" }
+        };
+
+
+
+    return(
+        <>
+        <section className="flex flex-col gap-5 max-w-6xl w-full mx-auto border-2 border-gray-200 p-6 rounded-2xl">
+            <header>
+                <h1 className="text-3xl">{title} #01</h1>
+                <label className="px-2 py-0.5 border rounded-xl font-bold text-white bg-green-600">Open</label>
+            </header>
+                <hr className={hrStyle} />
+
+
+            <div className="flex gap-8">
+
+
+                <div className="w-[70%]">
+                    <div className="flex gap-5">
+                        <label className="block border border-gray-300 bg-gray-200 p-2 rounded-full h-12 w-12 shrink-0"></label>
+                        <section className="border border-blue-200 rounded-xl overflow-hidden w-full">
+                            <div className="flex gap-2 px-5 bg-blue-100 p-2 border-b border-blue-200">
+                                <p>wilhelm-rolstad</p>
+                                <p className="text-gray-500">opened {date.toLocaleDateString()}</p>
+                                <p className="flex items-center text-sm border px-2 rounded-xl border-blue-200 ml-auto">Owner</p>
+                            </div>
+                            <div className="p-5">
+                                <p>{description}</p>
+                                <img src={imagePath} alt="ShowCaseImage" className="w-20 h-20 border"/>
+                            </div>
+                        </section>
+                    </div>
+                    <div>
+                        comitt-section ...
+                    </div>
+                </div>
+
+
+                <div className="flex flex-col gap-5 w-[30%]">
+                    <div className={infoContainer}>
+                        <p className={labelStyle}>Assignees</p>
+                        <div className="flex gap-2 items-center">
+                        <label className="block border border-gray-300 bg-gray-200 p-2 rounded-full h-5 w-5 shrink-0"/>
+                        <p>{assignee}</p>
+                        </div>
+                    </div>
+                    <hr className={hrStyle}/>
+                    <div className={infoContainer}>
+                        <p className={labelStyle}>Labels</p>
+                        <div className="flex flex-wrap gap-2">
+                        {labels.map((label) => {
+                            const style = labelStyles[label.toLowerCase()];
+
+                            return (
+                                <p
+                                key={label}
+                                className={`rounded-2xl px-3 py-0.5 font-bold ${
+                                    style ? `${style.bg} text-${style.text}` : "bg-pink-100 border-pink-200"
+                                }`}
+                                >
+                                {label}
+                                </p>
+                            );
+                            })}
+                        </div>
+                    </div>
+                    <hr className={hrStyle}/>
+                    <div className={infoContainer}>
+                        <p className={labelStyle}>Projects</p>
+                        <p>{project}</p>
+                    </div>
+                    <hr className={hrStyle}/>
+                    <div className={infoContainer}>
+                        <p className={labelStyle}>Milestone</p>
+                         <p>{milestone}</p>
+                    </div>
+                    <hr className={hrStyle}/>
+                </div>
+
+
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default GitHubCard
